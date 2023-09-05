29d16c521dccc457cce0bd59aa2d90aaaa2176ac
murderteeth <murdertxxth@gmail.com>
2023-09-05 07:06:48 +0000

    Add estimateCreationBlock

--- lib/blocks.ts
+++ lib/blocks.ts
@@ -2,14 +2,14 @@ import { PublicClient } from 'viem'
 
 export async function estimateHeight(rpc: PublicClient, timestamp: number) {
   try {
-    return await llama(rpc, timestamp)
+    return await estimateHeightLlama(rpc, timestamp)
   } catch(error) {
     console.log('🤬', error)
-    return await manual(rpc, timestamp)
+    return await estimateHeightManual(rpc, timestamp)
   }
 }
 
-async function llama(rpc: PublicClient, timestamp: number) {
+async function estimateHeightLlama(rpc: PublicClient, timestamp: number) {
 	const chain = (rpc.chain?.name as string).toLowerCase()
 	const response = await fetch(`https://coins.llama.fi/block/${chain}/${timestamp}`, {
 		headers: {accept: 'application/json'}
@@ -18,7 +18,7 @@ async function llama(rpc: PublicClient, timestamp: number) {
 	return height;
 }
 
-async function manual(rpc: PublicClient, timestamp: number) {
+async function estimateHeightManual(rpc: PublicClient, timestamp: number) {
   const top = await rpc.getBlock()
   let hi = top.number
   let lo = 0n
@@ -35,4 +35,23 @@ async function manual(rpc: PublicClient, timestamp: number) {
   }
 
   return block.number
+}
+
+// use bin search to estimate contract creat block
+// doesn't account for CREATE2 or SELFDESTRUCT
+// adapted from https://github.com/BobTheBuidler/ypricemagic/blob/5ba16b25302b47539b4e5a996554ba4c0a70e7c7/y/contracts.py#L68
+export async function estimateCreationBlock(rpc: PublicClient, contract: `0x${string}`) {
+  let counter = 0
+  console.time()
+  const height = await rpc.getBlockNumber()
+  let lo = 0n, hi = height, mid = lo + (hi - lo) / 2n
+  while (hi - lo > 1n) {
+    const bytecode = await rpc.getBytecode({ address: contract, blockNumber: mid })
+    if(!bytecode || bytecode.length === 0) { lo = mid } else { hi = mid }
+    mid = lo + (hi - lo) / 2n
+    counter++
+  }
+  console.log('💥', 'estimateCreationBlock', rpc.chain?.id, contract, counter, hi)
+  console.timeEnd()
+  return await rpc.getBlock({ blockNumber: hi })
 }
