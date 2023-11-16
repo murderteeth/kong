ad997f52ba685bf6568e506eef5eda0ced849eba
murderteeth <murdertxxth@gmail.com>
2023-11-16 02:38:48 +0000

    Cache gql responses

--- packages/gql/index.ts
+++ packages/gql/index.ts
@@ -1,40 +1,41 @@
 require('lib/json.monkeypatch')
 import path from 'path'
 import dotenv from 'dotenv'
-import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
 import { Monitor } from './monitor'
 import typeDefs from './typeDefs'
 import db from './db'
 import resolvers from './resolvers'
 import { cache } from 'lib'
 import { ProcessorPool } from 'lib/processor'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { ApolloServerPluginCacheControl } from '@apollo/server/plugin/cacheControl'
+import responseCachePlugin from '@apollo/server-plugin-response-cache'
 
 const envPath = path.join(__dirname, '../..', '.env')
 dotenv.config({ path: envPath })
 
-const port = process.env.GQL_PORT || 3001
+const port = Number(process.env.GQL_PORT || 3001)
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
-  cache: 'bounded'
+  plugins: [
+    ApolloServerPluginCacheControl({ defaultMaxAge: 60 }),
+    responseCachePlugin()
+  ]
 })
 
 export const monitors = new ProcessorPool(Monitor, 1, 600_000)
 
 Promise.all([
   cache.up(),
-  server.start(),
+  startStandaloneServer(server, { listen: { port } }),
   monitors.up()
 ]).then(() => {
 
-  const app = express()
-  server.applyMiddleware({ app })
-  app.listen(port, () => {
-    console.log(`🐒 gql up (${port})`)
-  })
+  console.log(`🐒 gql up (${port})`)
 
 }).catch(error => {
   console.error(error)
--- packages/gql/typeDefs/index.ts
+++ packages/gql/typeDefs/index.ts
@@ -1,4 +1,4 @@
-import { gql } from 'apollo-server-express'
+import gql from 'graphql-tag'
 import latestBlock from './latestBlock'
 import strategy from './strategy'
 import vault from './vault'
@@ -14,17 +14,29 @@ import stats from './stats'
 const query = gql`
   scalar BigInt
 
+  enum CacheControlScope {
+    PUBLIC
+    PRIVATE
+  }
+
+  directive @cacheControl(
+    maxAge: Int
+    scope: CacheControlScope
+    inheritMaxAge: Boolean
+  ) on FIELD_DEFINITION | OBJECT | INTERFACE | UNION
+
   type Query {
-    bananas: String,
-    latestBlocks(chainId: Int): [LatestBlock],
-    vaults(chainId: Int): [Vault],
-    vault(chainId: Int!, address: String!): Vault,
-    tvls(chainId: Int!, address: String!, period: Period, limit: Int): [Tvl],
-    apys(chainId: Int!, address: String!, period: Period, limit: Int): [Apy],
-    harvests(chainId: Int, address: String): [Harvest],
-    transfers(chainId: Int, address: String): [Transfer],
-    monitor: MonitorResults,
-    stats: Stats,
+    bananas: String @cacheControl(maxAge: 0)
+    latestBlocks(chainId: Int): [LatestBlock] @cacheControl(maxAge: 2)
+    monitor: MonitorResults @cacheControl(maxAge: 2)
+    stats: Stats @cacheControl(maxAge: 2)
+
+    vaults(chainId: Int): [Vault]
+    vault(chainId: Int!, address: String!): Vault
+    tvls(chainId: Int!, address: String!, period: Period, limit: Int): [Tvl]
+    apys(chainId: Int!, address: String!, period: Period, limit: Int): [Apy]
+    harvests(chainId: Int, address: String): [Harvest]
+    transfers(chainId: Int, address: String): [Transfer]
   }
 `
 
