49cb5428ab51d8f7552f4ddcd8ab556faf3bc886
murderteeth <89237203+murderteeth@users.noreply.github.com>
2024-01-16 19:44:06 -1000

    Keep last good tvl (#52)

--- packages/ingest/load/index.spec.ts
+++ packages/ingest/load/index.spec.ts
@@ -3,7 +3,8 @@ import { polygon, mainnet } from 'viem/chains'
 import { types } from 'lib'
 import { firstRow } from '../db'
 import { addresses } from '../test.fixture'
-import { upsertAsOfBlock } from '.'
+import { upsertAsOfBlock, upsertTvl } from '.'
+import { zeroAddress } from 'viem'
 
 describe('load', function() {
   it('upserts updates as of block number', async function() {
@@ -30,5 +31,36 @@ describe('load', function() {
 
     ;({ name } = await firstRow(`SELECT name FROM vault WHERE chain_id = $1 AND address = $2`, [polygon.id, addresses.v3.yvusdca]))
     expect(name).to.eq('2')
+  })
+
+  it.only('keeps last good TVL', async function() {
+    const goodTvl = {
+      chainId: 1,
+      address: zeroAddress,
+      priceUsd: 1.00,
+      priceSource: 'test',
+      tvlUsd: 1_000_000,
+      blockNumber: 1,
+      blockTime: 1
+    }
+
+    const badTvl = {
+      ...goodTvl,
+      priceUsd: 0.00,
+      priceSource: 'none',
+      tvlUsd: 0,
+    }
+
+    await upsertTvl(badTvl)
+    let { tvl_usd } = await firstRow(`SELECT tvl_usd FROM tvl where chain_id = $1 AND address = $2`, [1, zeroAddress])
+    expect(tvl_usd).to.eq(0)
+
+    await upsertTvl(goodTvl)
+    ;({ tvl_usd } = await firstRow(`SELECT tvl_usd FROM tvl where chain_id = $1 AND address = $2`, [1, zeroAddress]))
+    expect(tvl_usd).to.eq(1_000_000)
+
+    await upsertTvl(badTvl)
+    ;({ tvl_usd } = await firstRow(`SELECT tvl_usd FROM tvl where chain_id = $1 AND address = $2`, [1, zeroAddress]))
+    expect(tvl_usd).to.eq(1_000_000)
   })
 })
