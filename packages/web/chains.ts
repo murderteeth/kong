b5a490fe037b69f493ebb744a3de53e5b82a6dfc
murderteeth <murdertxxth@gmail.com>
2024-04-07 01:49:04 +0000

    Add gnosis to dash

--- packages/web/chains.ts
+++ packages/web/chains.ts
@@ -1,7 +1,7 @@
-import { arbitrum, base, fantom, mainnet, optimism, polygon } from 'viem/chains'
+import { arbitrum, base, fantom, gnosis, mainnet, optimism, polygon } from 'viem/chains'
 
 const chains = [
-  mainnet, optimism, polygon, fantom, base, arbitrum 
+  mainnet, optimism, gnosis, polygon, fantom, base, arbitrum 
 ]
 
 export default chains
