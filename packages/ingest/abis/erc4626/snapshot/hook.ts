a4b6b8eafc195ed4bbda389e62f8853dcef6a5d6
murderteeth <murdertxxth@gmail.com>
2024-05-25 00:54:18 +0000

    Add latest tvl and apy to generic 4626 snapshot

--- packages/ingest/abis/erc4626/snapshot/hook.ts
+++ packages/ingest/abis/erc4626/snapshot/hook.ts
@@ -2,14 +2,27 @@ import { EvmAddressSchema, ThingSchema } from 'lib/types'
 import { z } from 'zod'
 import { fetchOrExtractErc20 } from '../../yearn/lib'
 import { mq } from 'lib'
+import { getLatestApy, getSparkline } from '../../../db'
 
 export default async function process(chainId: number, address: `0x${string}`, data: any) {
   const { asset } = z.object({ asset: EvmAddressSchema }).parse(data)
+
+  const sparklines = {
+    tvl: await getSparkline(chainId, address, 'tvl'),
+    apy: await getSparkline(chainId, address, 'apy-bwd-delta-pps-generic', 'net')
+  }
+
+  const apy = await getLatestApy(chainId, address)
 
   const erc20 = await fetchOrExtractErc20(chainId, asset)
   await mq.add(mq.job.load.thing, ThingSchema.parse({
     chainId, address: asset, label: 'erc20', defaults: erc20
   }))
 
-  return { asset: erc20 }
+  return { 
+    asset: erc20,
+    sparklines,
+    tvl: sparklines.tvl[0],
+    apy
+  }
 }
