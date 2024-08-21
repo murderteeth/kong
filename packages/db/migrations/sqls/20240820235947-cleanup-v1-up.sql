0294523c8fd384719ce75e57c10238dbfd2cbbb1
murderteeth <89237203+murderteeth@users.noreply.github.com>
2024-08-20 20:40:09 -0400

    Chores (#150)
    * Upgrade dev pgdb to 16
    
    * Cleanup v1 tables

---
+++ packages/db/migrations/sqls/20240820235947-cleanup-v1-up.sql
@@ -0,0 +1,17 @@
+DROP VIEW harvest_gql;
+DROP VIEW strategy_gql;
+DROP VIEW vault_gql;
+
+DROP TABLE apr;
+DROP TABLE apy;
+DROP TABLE erc20;
+DROP TABLE harvest;
+DROP TABLE risk_group;
+DROP TABLE sparkline;
+DROP TABLE strategy;
+DROP TABLE strategy_lender_status;
+DROP TABLE transfer;
+DROP TABLE tvl;
+DROP TABLE vault;
+DROP TABLE vault_debt;
+DROP TABLE withdrawal_queue;
