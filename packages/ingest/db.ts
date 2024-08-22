39c65d6437c09577e12ba4862bbe1b9d45319b37
murderteeth <89237203+murderteeth@users.noreply.github.com>
2024-08-21 23:33:33 -0400

    Allow rejectUnauthorized (#153)

--- packages/ingest/db.ts
+++ packages/ingest/db.ts
@@ -13,12 +13,12 @@ pgTypes.setTypeParser(1184, (stringValue) => {
 })
 
 const db = new Pool({
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: (process.env.POSTGRES_PORT || 5432) as number,
-  ssl: (process.env.POSTGRES_SSL || false) as boolean,
-  database: process.env.POSTGRES_DATABASE || 'user',
-  user: process.env.POSTGRES_USER || 'user',
-  password: process.env.POSTGRES_PASSWORD || 'password',
+  host: process.env.POSTGRES_HOST ?? 'localhost',
+  port: (process.env.POSTGRES_PORT ?? 5432) as number,
+  ssl: (process.env.POSTGRES_SSL ?? false) ? true : { rejectUnauthorized: false },
+  database: process.env.POSTGRES_DATABASE ?? 'user',
+  user: process.env.POSTGRES_USER ?? 'user',
+  password: process.env.POSTGRES_PASSWORD ?? 'password',
   max: 40,
   idleTimeoutMillis: 10_000,
   connectionTimeoutMillis: 60_000,
--- packages/web/app/api/db/index.ts
+++ packages/web/app/api/db/index.ts
@@ -6,12 +6,12 @@ pgTypes.setTypeParser(1184, (stringValue) => {
 })
 
 const db = new Pool({
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: (process.env.POSTGRES_PORT || 5432) as number,
-  ssl: (process.env.POSTGRES_SSL || false) as boolean,
-  database: process.env.POSTGRES_DATABASE || 'user',
-  user: process.env.POSTGRES_USER || 'user',
-  password: process.env.POSTGRES_PASSWORD || 'password',
+  host: process.env.POSTGRES_HOST ?? 'localhost',
+  port: (process.env.POSTGRES_PORT ?? 5432) as number,
+  ssl: (process.env.POSTGRES_SSL ?? false) as boolean,
+  database: process.env.POSTGRES_DATABASE ?? 'user',
+  user: process.env.POSTGRES_USER ?? 'user',
+  password: process.env.POSTGRES_PASSWORD ?? 'password',
   max: 4,
   idleTimeoutMillis: 60_000,
   connectionTimeoutMillis: 5_000,
