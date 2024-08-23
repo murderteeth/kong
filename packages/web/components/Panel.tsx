066c746e0653db2c816bbe793dc8140a00bf5fd4
murderteeth <murdertxxth@gmail.com>
2024-08-23 06:09:55 +0000

    Clean up vault components

--- packages/web/components/Panel.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from 'react'
-
-export default function Panel({ className, children }: { className?: string, children: ReactNode }) {
-  return <div className={`p-2 sm:p-4 ${className}`}>
-    {children}
-  </div>
-}
